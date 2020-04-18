8054dde342df2f38fd99a80531b4e09e1327c723
Daniel <dgarwood@uncc.edu>
2020-04-18 10:21:31 -0400

    Add authentication

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AuthGuard } from './auth/auth.guard';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -7,6 +8,7 @@ dotenv.config();
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
+    app.useGlobalGuards(new AuthGuard());
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
